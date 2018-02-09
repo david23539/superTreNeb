596afa426bc0d1ea8aea728467232e0c8323d92d
david23539 <david23539@gmail.com>
2018-02-09 19:14:47 +0100

    SOLUCIONADO FIX EN REGEX FALTA falta la logica de desbloquear a un usuario cambiar modal por toast

--- Frontend/SupeTrejNeb/src/app/validate/email.validate.ts
+++ Frontend/SupeTrejNeb/src/app/validate/email.validate.ts
@@ -3,8 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class emailValidate{
 
+  public regexp:any;
+
   validate(email):Boolean{
-    let regexp = new RegExp('^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
-    return regexp.test(email);
+
+    this.regexp = new RegExp('^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$');
+    return this.regexp.test(email);
   }
 }
