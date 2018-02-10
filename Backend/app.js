23b404fb98524661b2d925540c20f8071432a38f
david23539 <david23539@gmail.com>
2018-02-10 18:38:45 +0100

    FALTA pantalla de bloquo de ip maquetarla
    fecha de entrega lunes 12

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const constantFile = require('./utils/Constant')
 const app = express()
+const path = require('path')
 //rutas
 const user_routes = require('./routes/user.route')
 const person_router = require('./routes/person.route')
@@ -24,10 +25,15 @@ app.use((req,res,next)=>{
 	res.header('Allow', 'GET, POST, PUT, OPTIONS, DELETE')
 	next()
 })
+
 //rutas base
+//app.use('/', express.static('client', {redirect:false}))
 app.use('/api', user_routes)
 app.use('/api', person_router)
 app.use('/api', direcctionIp_router)
+/*app.get('*', function(req, res, next){
+	res.sendFile(path.resolve('client/index.html'))
+})*/
 //app.use('/api', animal_routes)
 
 //rutas body-parse
--- Frontend/SupeTrejNeb/src/app/services/global.ts
+++ Frontend/SupeTrejNeb/src/app/services/global.ts
@@ -1,3 +1,4 @@
 export const GLOBAL = {
   url:"http://localhost:3789/api/"
+ // url:"http://davizco-tech.es/api/"
 };
