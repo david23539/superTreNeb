6ed14d5305eecac2b88ab22853afc83d6b8f644b
david <david23539@gmail.com>
2019-04-15 12:03:03 +0200

    se soluciona el problema de descarga de facturas

--- Backend/app.js
+++ Backend/app.js
@@ -39,8 +39,8 @@ app.disable('x-powered-by');
 
 //configurar cabeceras y cors
 app.use((req,res,next)=>{
-	res.header('Access-Control-Allow-Origin', '159.89.25.242');
-	// res.header('Access-Control-Allow-Origin', '*');
+	// res.header('Access-Control-Allow-Origin', '159.89.25.242');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.header('Allow', 'GET, POST, PUT, DELETE')
