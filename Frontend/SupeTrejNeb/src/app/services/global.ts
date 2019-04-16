96f4e968e8e5401a7c47307d4dc553db979020ff
david <david23539@gmail.com>
2019-04-16 12:45:29 +0200

    se rectaura los parametros de las conexiones

--- Frontend/SupeTrejNeb/src/app/services/global.ts
+++ Frontend/SupeTrejNeb/src/app/services/global.ts
@@ -1,4 +1,4 @@
 export const GLOBAL = {
-  url:"http://localhost:3789/api/"
-  // url:"http://davizco-tech.es/api/"
+  // url:"http://localhost:3789/api/"
+  url:"http://autoserviciolalla.es/api/"
 };
